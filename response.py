570f14fdb15a59590641f987c7f331dfb23dc8ad
Joe Thor <jfthor@umn.edu>
2018-05-31 10:25:28 -0500

    functions are working now. Need to double check formatting

--- response.py
+++ response.py
@@ -4,15 +4,26 @@ fileName = name + ".txt"
 
 file_1= open(fileName, "w")
 
-voucherOrReceipt = raw_input("Is the PO missing a voucher or receipt: ")
+voucherOrReceipt = raw_input("Is the PO missing a voucher[1] or receipt[2]? Please enter 1 or 2:  ")
 
-def noVoucher (): 
+
+def whichToRun():
+	global voucherOrReceipt
+	print voucherOrReceipt
+	if voucherOrReceipt == "1":
+		noVoucher()
+	
+	elif voucherOrReceipt == "2":
+		noReceipt()
+	
+	else:
+		print "run this again, you chose wrong"
+
+def noVoucher(): 
+
+	firstLine = "Hello," + "\n"
 
 	PONumber = raw_input("PO Number: ")
-
-	receipt = raw_input("Receipt Number: ")
-
-	receiptDate = raw_input("Receipt Date: ")
 
 	supplierName = raw_input("supplier name: ")
 
@@ -20,27 +31,35 @@ def noVoucher ():
 
 	preparersLastName = raw_input("Preparer's Last Name: ")
 	
-	firstLine = "Hello," + "\n"
+	receipt = raw_input("Receipt Number: ")
 
+	receiptDate = raw_input("Receipt Date: ")
 
-	message = "Supplier " + supplierName + " reached out to the EFS helpline today concerning payment from PO " + PONumber + ". The PO sourced from a req created by " + preparersFirstName + " " + preparersLastName + " like the PO has been received against by " + preparersFirstName + " via receipt " + receipt + " on " +  receiptDate + ". However, not voucher exists on the system. not voucher exists on the system. If you have the invoice still, please send it to your cluster for imaging. Otherwise, if you need the invoice, I have CC'd the supplier on this email so they should be able to facilitate this. Let us know if we can be of any help, "
+	message = "Supplier " + supplierName + " reached out to the EFS helpline today concerning payment from PO " + PONumber + ". The PO sourced from a req created by " + preparersFirstName + " " + preparersLastName + ". It looks like the PO has been received against by " + preparersFirstName + " via receipt " + receipt + " on " +  receiptDate + ". However, not voucher exists on the system. not voucher exists on the system. If you have the invoice still, please send it to your cluster for imaging. Otherwise, if you need the invoice, I have CC'd the supplier on this email so they should be able to facilitate this. Let us know if we can be of any help, "
 
 
 	file_1.write(firstLine + message)
+	
+	print "Success!"
 
-def noReceipt:
+def noReceipt():
 
-	
-	
-def whichToRun():
-	
-	if voucherOrReceipt === "voucher":
-		noVoucher()
-	
-	elif voucherOrReceipt === "receipt":
+	firstLine = "Hello," + "\n"
 
-		noReceipt()
+	PONumber = raw_input("PO Number: ")
+
+	supplierName = raw_input("supplier name: ")
+
+	preparersFirstName = raw_input("Preparer's first Name: ")
+
+	preparersLastName = raw_input("Preparer's Last Name: ")
+	 
+	voucher = raw_input("voucher number: ")
 	
-	else:
-		voucherOrReceipt = raw_input("")
-		whichToRun()
+	message = "Supplier " + supplierName + " reached out to the EFS helpline today concerning payment from PO " + PONumber + "The PO sourced from a req created by " + preparersFirstName + " " + preparersLastName + "There is a voucher created against this PO, voucher " + voucher + ". However, no receipt has been created against the PO."+ preparersFirstName + ", please go in and receive against the PO. Once a receipt has been created, the system will match the voucher and a payment should go out. If you need an invoice, I recommend you reach out the supplier who I have CC'd to this email. Otherwise, if you have questions or concerns about the receiving process, please let me know. Have a great day."
+	
+	file_1.write(firstLine + message)
+	
+	print "Success!"
+
+whichToRun()
